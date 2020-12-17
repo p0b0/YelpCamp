ac3d8fe3674a5b94c2eed061b0ce3343320b6bcd
p0b0 <d_darv@hotmail.gr>
2020-12-17 10:33:48 +0200

    created db session store

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const localStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const MongoStore = require('connect-mongo')(session);
 
 
 const localDbPath = 'mongodb://localhost:27017/yelpcamp';
@@ -51,7 +52,21 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
+
+
+const store = new MongoStore({
+	url: localDbPath,
+	secret: 'thisshouldbeabettersecret',
+	touchAfter: 24 * 60 * 60
+})
+
+store.on('error', function(e) {
+	console.log("session store error!", e)
+})
+
+
 const sessionConfig = {
+	store,
 	name: 'session',
 	secret: 'thisshouldbeabettersecret',
 	resave: false,
