1cb236a9f4d8bbdc71dbcb488bc8e3faab84d56a
p0b0 <d_darv@hotmail.gr>
2020-12-17 08:33:05 +0200

    configured content security policy

--- app.js
+++ app.js
@@ -58,56 +58,58 @@ app.use(mongoSanitize());
 
 
 
-app.use(
-	helmet({
-	  contentSecurityPolicy: false,
-	})
-  );
-
-
-// const scriptSrcUrls = [
-//     "https://stackpath.bootstrapcdn.com",
-//     "https://api.tiles.mapbox.com",
-//     "https://api.mapbox.com",
-//     "https://kit.fontawesome.com",
-//     "https://cdnjs.cloudflare.com",
-//     "https://cdn.jsdelivr.net",
-// ];
-// const styleSrcUrls = [
-//     "https://kit-free.fontawesome.com",
-//     "https://stackpath.bootstrapcdn.com",
-//     "https://api.mapbox.com",
-//     "https://api.tiles.mapbox.com",
-//     "https://fonts.googleapis.com",
-//     "https://use.fontawesome.com",
-// ];
-// const connectSrcUrls = [
-//     "https://api.mapbox.com",
-//     "https://*.tiles.mapbox.com",
-//     "https://events.mapbox.com",
-// ];
-// const fontSrcUrls = [];
 // app.use(
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             defaultSrc: [],
-//             connectSrc: ["'self'", ...connectSrcUrls],
-//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-//             workerSrc: ["'self'", "blob:"],
-//             childSrc: ["blob:"],
-//             objectSrc: [],
-//             imgSrc: [
-//                 "'self'",
-//                 "blob:",
-//                 "data:",
-//                 "https://res.cloudinary.com/p0b0yelp/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-//                 "https://images.unsplash.com",
-//             ],
-//             fontSrc: ["'self'", ...fontSrcUrls],
-//         },
-//     })
-// );
+// 	helmet({
+// 	  contentSecurityPolicy: false,
+// 	})
+//   );
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+	"https://cdn.jsdelivr.net",
+	"https://code.jquery.com/jquery-3.5.1.slim.min.js"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+	"https://use.fontawesome.com",
+	"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/p0b0yelp/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(session(sessionConfig));
 app.use(flash());
